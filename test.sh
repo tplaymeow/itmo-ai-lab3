58a1ca9e609afd3352ec552de39c5f4c0cd0a2b3
tplaymeow <wersa2002@gmail.com>
2023-09-25 07:24:10 +0300

    fixed tests

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 OUTPUT=$(python3 main.py monopoly.pl < test_requests.txt)
 EXPECTED_OUTPUT=$(cat test_responses.txt)
-if [ "$OUTPUT" == "$EXPECTED_OUTPUT" ]
+if [ "$OUTPUT" = "$EXPECTED_OUTPUT" ]
 then
   echo "Tests OK"
 else
